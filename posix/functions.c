de8842e27cf4d023a8f9e1101890a7104f728cff
Peter Barker <pbarker@barker.dropbear.id.au>
2017-06-14 11:38:49 +1000

    posix: add file_listdir

--- posix/functions.c
+++ posix/functions.c
@@ -4,6 +4,9 @@
 
 #include <sys/statvfs.h>
 
+#include <sys/types.h>
+#include <dirent.h>
+
 static void sock_send_diskinfo(struct sock_buf *sock, const char *label, unsigned serial, unsigned total_clusters, unsigned free_clusters, unsigned cluster_size)
 {
     sock_printf(sock,
@@ -36,7 +39,74 @@ static void disk_info(struct template_state *tmpl, const char *name, const char 
 }
 
 
+static void sock_send_dirent(struct sock_buf *sock, bool first, int type, const char *name, unsigned year, unsigned month, unsigned day, unsigned hour, unsigned minute, unsigned second, unsigned size)
+{
+    if (!first) {
+        sock_printf(sock, "%s", ",\n");
+    }
+    sock_printf(sock, "{"
+                "\"type\" : %u, "
+                "\"name\" : \"%s\", "
+                "\"date\" : \"%04u-%02u-%02u %02u:%02u:%02u\", "
+                "\"size\" : %u "
+                "}",
+                type, name, year, month, day, hour, minute, second, size);
+}
+
+static void file_listdir(struct template_state *tmpl, const char *name, const char *value, int argc, char **argv)
+{
+    if (argc < 1) {
+        return;
+    }
+    const char *dirpath = argv[0];
+
+    DIR *dh = opendir(dirpath);
+    if (dh == NULL) {
+        console_printf("Failed to open directory %s\n", dirpath);
+        return;
+    }
+
+    sock_printf(tmpl->sock, "[ ");
+
+    bool first = true;
+    struct dirent *result;
+    while ((result = readdir(dh)) != NULL) {
+        if (strcmp(result->d_name, ".") == 0) {
+            continue;
+        }
+
+        struct stat buf;
+        char filepath[PATH_MAX] = {};
+        snprintf(filepath, sizeof(filepath), "%s/%s", dirpath, result->d_name);
+        if (stat(filepath, &buf) == -1) {
+            continue;
+        }
+
+        struct tm t;
+        if (localtime_r(&buf.st_mtim.tv_sec, &t) == NULL) {
+            continue;
+        }
+
+        sock_send_dirent(tmpl->sock,
+                         first,
+                         (result->d_type==DT_DIR) ? 1 :0,
+                         result->d_name,
+                         t.tm_year+1900,
+                         t.tm_mon+1,
+                         t.tm_mday,
+                         t.tm_hour,
+                         t.tm_min,
+                         t.tm_sec,
+                         buf.st_size);
+        first = false;
+    }
+    sock_printf(tmpl->sock, "]");
+    closedir(dh);
+}
+
+
 void posix_functions_init(struct template_state *tmpl)
 {
+    tmpl->put(tmpl, "file_listdir", "", file_listdir);
     tmpl->put(tmpl, "disk_info", "", disk_info);
 }
