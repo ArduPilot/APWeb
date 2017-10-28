949b8df1b0a03f85d7a4f3cc602127bd8e872673
Peter Barker <pbarker@barker.dropbear.id.au>
2017-10-28 13:59:26 +1100

    include.h: FreeRTOS support
    Originally authored by tridge

--- includes.h
+++ includes.h
@@ -1,8 +1,30 @@
 #pragma once
 
+#ifdef SYSTEM_FREERTOS
+#include <FreeRTOS.h>
+#include <bsp.h>
+#include <task.h>
+#include <nonstdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <libmid_fatfs/ff.h>
+#include "socket_ctrl.h"
+#include <generated/snx_sdk_conf.h>
+#include "../talloc.h"
+#include "../dev_console.h"
+#include "../util/print_vprintf.h"
+#else
 #include "linux/includes.h"
+#include "mavlink_core.h"
+#endif
 
 #include "cgi.h"
 #include "template.h"
 #include "web_files.h"
-#include "mavlink_core.h"
+
+
